8aa13271f66503e312c164cc6ace851e9c1086e5
Christy Guan <cguan00@stuy.edu>
2019-03-15 14:45:58 -0400

    moved pseudo code to helper method

--- Merge.java
+++ Merge.java
@@ -1,19 +1,20 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[] data){
-    int lo = 0;//start at index 0
-    int hi = data.length - 1;//end at last index
-    int mid = data.length / 2;//middle index of data array
-    mergesort(data, lo, hi);//call helper method on whole array
-    if(lo >= hi){
-      return;//void method, so don't return anything
-    }
-    mergesort(data, 0, mid - 1);// mergesort left side
-    mergesort(data, mid + 1, hi);// mergesort right side
-    // merge
+    int lo = 0;
+    int hi = data.length - 1;
+    mergesort(data, lo, hi);//call to helper method
   }
 
   public static void mergesort(int[] data, int lo, int hi){
+    if(lo >= hi){
+      return;//void method, so don't return anything
+    }
+    int mid = data.length / 2;//middle index of data array
+    mergesort(data, 0, mid - 1);// mergesort left side
+    mergesort(data, mid + 1, hi);// mergesort right side
+    // merge
+
 
   }
 
