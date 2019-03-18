29bfaf17ac2ca4af0cb34c94884677c32833ba9f
Christy Guan <cguan00@stuy.edu>
2019-03-17 23:03:11 -0400

    testing methods

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+  public static void main(String[] args) {
+    int[] data1 = {20, 30, 15, 10, 0, 50};
+    System.out.println(Merge.printArray(data1));
+    // Merge.mergesort(data1);
+    // System.out.println(Merge.printArray(data1));
+  }
+}
