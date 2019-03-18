df40312f47a2944290af5c4782929573f3143e71
Christy Guan <cguan00@stuy.edu>
2019-03-17 23:51:27 -0400

    tested mergesort

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver{
   public static void main(String[] args) {
     int[] data1 = {20, 30, 15, 10, 0, 50};
     System.out.println(Merge.printArray(data1));
-    // Merge.mergesort(data1);
-    // System.out.println(Merge.printArray(data1));
+    Merge.mergesort(data1);
+    System.out.println(Merge.printArray(data1));
   }
 }
