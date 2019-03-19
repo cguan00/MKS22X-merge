5e7986b50785ab8e72028614959907071b828de7
Christy Guan <cguan00@stuy.edu>
2019-03-19 14:46:58 -0400

    copied driver ffrom Mr. K's website

--- Merge.java
+++ Merge.java
@@ -1,6 +1,11 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class Merge{
+  private static final int INCREASE = 0;
+  private static final int DECREASE = 1;
+  private static final int STANDARD = 2;
+  private static final int SMALL_RANGE = 3;
+
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[] data){
     int lo = 0;
@@ -100,40 +105,84 @@ public class Merge{
     return output;
   }
 
-  public static void main(String[]args){
-   System.out.println("Size\t\tMax Value\tmerge/builtin ratio ");
-   int[]MAX_LIST = {1000000000,500,10};
-   for(int MAX : MAX_LIST){
-     for(int size = 31250; size < 2000001; size*=2){
-       long qtime=0;
-       long btime=0;
-       //average of 5 sorts.
-       for(int trial = 0 ; trial <=5; trial++){
-         int []data1 = new int[size];
-         int []data2 = new int[size];
-         for(int i = 0; i < data1.length; i++){
-           data1[i] = (int)(Math.random()*MAX);
-           data2[i] = data1[i];
-         }
-         long t1,t2;
-         t1 = System.currentTimeMillis();
-         Merge.mergesort(data2);
-         t2 = System.currentTimeMillis();
-         qtime += t2 - t1;
-         t1 = System.currentTimeMillis();
-         Arrays.sort(data1);
-         t2 = System.currentTimeMillis();
-         btime+= t2 - t1;
-         if(!Arrays.equals(data1,data2)){
-           System.out.println("FAIL TO SORT!");
-           System.exit(0);
-         }
-       }
-       System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-     }
-     System.out.println();
-   }
+  public static void insertionsort(int[] data){
+    insertionsort(data, 0, data.length - 1);
   }
+
+  public static void insertionsort(int[] data, int lo, int hi){
+    if(lo >= hi){
+      return;//base case
+    }
+    int mid = (data.length + 1) / 2;
+  }
+
+  private static String name(int i){
+    if(i==INCREASE)return "Increassing";
+    if(i==DECREASE)return "Decreassing";
+    if(i==STANDARD)return "Normal Random";
+    if(i==SMALL_RANGE)return "Random with Few Values";
+    return "Error categorizing array";
+  }
+
+  private static int create(int min, int max){
+    return min + (int)(Math.random()*(max-min));
+  }
+
+  private static int[]makeArray(int size,int type){
+    int[]ans =new int[size];
+    if(type == STANDARD){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-1000000,1000000);
+      }
+    }
+    else if(type == INCREASE){
+      int current = -5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current += 10;
+      }
+    }
+    else if(type == DECREASE){
+      int current = 5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current -= 10;
+      }
+    }
+    else if(type == SMALL_RANGE){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-5,5);
+      }
+    }
+    else{
+      ans = new int[0];//empty is default
+    }
+    return ans;
+  }
+
+  public static void main(String[]args){
+     if(args.length < 2)return;
+
+     int size =  Integer.parseInt(args[0]);
+     int type =   Integer.parseInt(args[1]);
+
+     int [] start = makeArray(size,type);
+     int [] result = Arrays.copyOf(start,start.length);
+     Arrays.sort(result);
+
+     long startTime = System.currentTimeMillis();
+     /*
+      * Test your sort here //yoursort(start);
+      * Add code to switch which sort is tested by changing one of the args!
+      */
+     long elapsedTime = System.currentTimeMillis() - startTime;
+     if(Arrays.equals(start,result)){
+       System.out.println("PASS Case "+name(type)+"\t array, size:"+start.length+"\t"+elapsedTime/1000.0+"sec ");
+     }else{
+       System.out.println("FAIL ! ERROR ! "+name(type)+" array, size:"+size+"  ERROR!");
+     }
+   }
+
 
 
 
